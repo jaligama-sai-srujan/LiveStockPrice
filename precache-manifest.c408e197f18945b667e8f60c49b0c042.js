f953d608d4c5e3081a8a0af34486610dca9283bb
jaligama-sai-srujan <saisrujan1998@gmail.com>
2020-06-21 18:30:06 -0700

    Updates

---
+++ precache-manifest.c408e197f18945b667e8f60c49b0c042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f189e90612ac4327471b94616c8c85c0",
+    "url": "/LiveStockPrice/index.html"
+  },
+  {
+    "revision": "7197a7d46da13468ef21",
+    "url": "/LiveStockPrice/static/css/main.9157ef8e.chunk.css"
+  },
+  {
+    "revision": "ed2b8a0580cb375c1a32",
+    "url": "/LiveStockPrice/static/js/2.2e587820.chunk.js"
+  },
+  {
+    "revision": "9e85adb46ccd889381ac204342d9aaa2",
+    "url": "/LiveStockPrice/static/js/2.2e587820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7197a7d46da13468ef21",
+    "url": "/LiveStockPrice/static/js/main.096ffb58.chunk.js"
+  },
+  {
+    "revision": "603691192a6551d35a10",
+    "url": "/LiveStockPrice/static/js/runtime-main.611df521.js"
+  }
+]);
